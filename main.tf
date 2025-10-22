6278583020a4ec5bac09299c3a227ba1cee21da8
pattarpg <admin@contoso.com>
2025-10-22 03:29:54 +0000

    resource_group_name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 } 
 
 resource "azurerm_network_security_group" "example_nsg" {
-    name                = "iac-devops-nsg-0992"
+    name                = "iac-devops-nsg"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }
